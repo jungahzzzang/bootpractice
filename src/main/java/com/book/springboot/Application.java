f93c8fdd7a38408ee50694f8cf51a7d71e248748
jungahzzzang <jungahzzzang@gmail.com>
2022-03-04 00:14:05 +0900

    커밋 테스트

--- src/main/java/com/book/springboot/Application.java
+++ src/main/java/com/book/springboot/Application.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  * 스프링부트의 자동 설정, 스프링 Bean읽기와 생성이 모두 자동으로 설정됨.
  * 이 어노테이션이 있는 위치부터 설정을 읽어가기 때문에 항상 프로젝트 최상단에 위치해야 함.
  */
-@EnableJpaAuditing  //JPA Autiting 활성화
+//@EnableJpaAuditing  //JPA Autiting 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
