63bb3ae87445d6e74d47850f56bb6f18cb047142
jungahzzzang <jungahzzzang@gmail.com>
2022-03-07 13:12:16 +0900

    간단 배포 테스트 4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,10 +8,12 @@ echo ">Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 echo ">현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl bootpractice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl flex-springboot2-webservice | grep jar | awk '{print $1}')
 echo ">현재 구동 중인 애플리케이션 pid:$CURRENT_PID"
 
 if [ -z "$CURRENT_PID"]; then
+  echo ">현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
   echo ">kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
